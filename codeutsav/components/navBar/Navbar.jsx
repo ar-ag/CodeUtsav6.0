e7148614a9ca336584984a2449907970176acb90
Manjeet Pathak <manjeetpathak2003@gmail.com>
2023-02-11 19:14:10 +0530

    added redirection

--- codeutsav/components/navBar/Navbar.jsx
+++ codeutsav/components/navBar/Navbar.jsx
@@ -6,11 +6,29 @@ import axios from 'axios'
 import { useWeb3Modal, Web3Button } from '@web3modal/react'
 import { useState } from 'react'
 import { useAccount, useDisconnect } from 'wagmi'
-
+import { useRouter } from 'next/router';
 const NavBar = () => {
+
+  const [logged,setLogged]=useState(false);
+  const {isConnected,address}=useAccount();
   async function getWallet(){
     const response=await axios.get('/api/hello');
   }
+  let router = useRouter()
+const Data=()=>{
+  if(!isConnected){
+    setLogged(true);
+    if(logged===true){
+      router.push('/patientList')
+    }
+    console.log(address);
+  }
+
+  if(!isConnected){
+    router.push('/')
+  }
+}
+
 
  return (
   <>
@@ -26,7 +44,7 @@ const NavBar = () => {
          {/* <button onClick={getWallet}>
            Connect Wallet
          </button> */}
-         <Web3Button icon="show" label="Connect Wallet" balance="show" />
+         <Web3Button icon="show" label="Connect Wallet" balance="show" onClick={Data}/>
          <div className='line'></div>
          <div className='settings'>
            <Image src={setting} />
